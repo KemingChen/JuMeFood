c7b5e1ee1a199332f3eb987b4ed66405f3c0adb0
gary62107 <gary62107@gmail.com.tw>
2014-06-11 01:14:17 +0800

    simple slot machine

--- js/controllers/TurntableCtrl.js
+++ js/controllers/TurntableCtrl.js
@@ -1,3 +1,8 @@
 app.controller('TurntableCtrl', function($scope, $rootScope, Core){
-	
+	$('.slot').jSlots({
+		spinner : '#playNormal',
+		winnerNumber : 7,
+		number : 1,
+		winnerIndex : 3
+	});
 });
