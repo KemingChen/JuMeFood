872d8e1c33b473ee412e7aee75e9308d7ab34654
gary62107 <gary62107@gmail.com.tw>
2014-06-12 05:56:50 +0800

    update

--- js/controllers/TurntableCtrl.js
+++ js/controllers/TurntableCtrl.js
@@ -4,16 +4,40 @@ app.controller('TurntableCtrl', function($scope, $rootScope, Core, $timeout, $io
 	console.log(JSON.stringify($scope.room));
 	$rootScope.$broadcast('EnterRoom', {room: $scope.room});
 
-	$timeout(function(){
-		$('.slot').jSlots({
-			spinner : '#playGo',
-			number : 1,
-			winnerIndex : 2,
-			onEnd: function(){
-				console.log("finish");
-			},
-		});
-	}, 100);
+	$scope.go = function(){
+		console.log('go');
+		if($scope.room.rid == "self")
+		{
+			$('.slot').jSlots({
+				spinner : '#playGo',
+				number : 1,
+				onEnd: function(result){
+					console.log("finish");
+					var store = $scope.room.advices[result[0]];
+					console.log(store);
+				},
+			});
+			$("#playGo").click();
+		}
+		else
+		{
+			/*
+			api.go(function(result){
+				$('.slot').jSlots({
+					spinner : '#playGo',
+					number : 1,
+					winnerIndex : result,
+					onEnd: function(result){
+						console.log("finish");
+						var store = $scope.room.advices[result[0]];
+						console.log(store);
+					},
+				});
+				$("#playGo").click();		
+			})
+			*/
+		}
+	}
 
 	$scope.isNoAdvices = function(){
 		var advices = $scope.room.advices;
